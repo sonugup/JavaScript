01607c95ec12b622a7ebacad0273832cd636c1ab
Sonu Gupta <rg3029926@gmail.com>
2023-01-22 04:01:31 +0530

    binary search

--- binariSearch.js
+++ binariSearch.js
@@ -1,27 +1,27 @@
-// function search(arr, k){
-//     let start=0;
-//     let end=arr.length-1;
-//     while(end>start){
-//         let mid= start+Math.floor((end-start)/2)
-//         if(arr[mid]==k){
-//             return mid;
-//         }
-//         else if(arr[mid]>k){
-//             end=mid-1;
-//         }
-//         else{
-//             start=mid+1
-//         }
-//         mid= start+Math.floor((end-start)/2)
-//         // console.log(mid)
-//     }
-// }
+function search(arr, k){
+    let start=0;
+    let end=arr.length-1;
+    while(end>start){
+        let mid= start+Math.floor((end-start)/2)
+        if(arr[mid]==k){
+            return mid;
+        }
+        else if(arr[mid]>k){
+            end=mid-1;
+        }
+        else{
+            start=mid+1
+        }
+        mid= start+Math.floor((end-start)/2)
+        // console.log(mid)
+    }
+}
 
 
-// let arr=[2, 4, 9, 6, 8, 12, 5, 7]
-// let k=6
+let arr=[2, 4, 6, 8, 12, 15]
+let k=9
 
-// console.log(search(arr, k))
+console.log(search(arr, k))
 
 
 // Recursive implementation of Binary Search:
