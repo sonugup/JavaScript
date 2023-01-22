69661a7c65c36492269e6d2c0b095f7f1e5dbee6
Sonu Gupta <rg3029926@gmail.com>
2023-01-23 00:55:04 +0530

    rain coat

---
+++ rain.js
@@ -0,0 +1,11 @@
+//if raining =raincoat
+// else no raincoat
+
+
+
+var tomr="rain"
+if(tomr=='rain'){
+    console.log('take a raincoat')
+}else{
+    console.log("No need to take a raincoat")
+}  
