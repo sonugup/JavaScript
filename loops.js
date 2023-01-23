0023bd7d1dfaa111a521a78e920b0d3f7e16b026
Sonu Gupta <rg3029926@gmail.com>
2023-01-23 16:47:15 +0530

    loop

---
+++ loops.js
@@ -0,0 +1,23 @@
+
+//while loop
+var num=0;
+while(num<=10){
+    console.log(num)
+    num++
+}
+
+//do while loop
+var num=0
+do{
+    console.log(num)
+    num++
+}while(num<=10)
+
+
+
+//for loop
+
+for(var i= 1; i<=10; i++){
+    var tab=8;
+    console.log(tab + "*" + i + "=" + tab*i)
+}
