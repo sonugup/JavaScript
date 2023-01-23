1959db7687ec5bd29c44fbbbe590b3303f3012cd
Sonu Gupta <rg3029926@gmail.com>
2023-01-23 23:34:42 +0530

    function

--- allfun.js
+++ allfun.js
@@ -33,16 +33,16 @@
 
 
 //parameter
-function functionName(a, b){
-    //Statement
-    // var a=10, b=20;
-    var sum=a+b;
-    console.log(sum)
-}
+// function functionName(a, b){
+//     //Statement
+//     // var a=10, b=20;
+//     var sum=a+b;
+//     console.log(sum)
+// }
 
-// console.log(functionName())
-        //Argument
-functionName(20, 30)
+// // console.log(functionName())
+//         //Argument
+// functionName(20, 30)
 
 // why function 
 // You can reuse code: Define the code oncuechange, and use it many times.
@@ -51,4 +51,54 @@ functionName(20, 30)
 // or 
 // A function is a group of resable which can be called anywhere
 // in your program. This eliminates the neet of writing the same code again and again.
+
+
+
+// 4- Function expression
+// function expression simply means
+// create a function and put it into the variabl
+
+
+// function sum (a, b){
+//     var total=a+b;
+//     console.log(total)
+// }
+
+// var funExp=sum(5 , 38);
+// funExp;
+
+
+/*
+Return keyword 
+when javascript reachs a return statement,
+the function will stop executing.
+Functions often compute a return value.
+the return value is "returned" back to the calle;
+*/
+
+function sum (a, b){
+    return a+b;
+}
+
+var funExp=sum(5 , 38);
+console.log(funExp);
+
+// Anonymous Function;
+
+// 6- A function expression is similar to and has the same syntax as a 
+// function declaration One can define "named"
+// function expressions (where the name of the expression might
+//     be used in the call stack for example)
+//     or "anonymous" function expressions.
+
+var funExp=function (a, b){
+    return total=a+b;
+}
+
+
+var sum= funExp(5 , 38);
+
+var sum1= funExp(13 , 38);
+console.log(sum>sum1)
+
 
