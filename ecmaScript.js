f52c1458ec7a89b8b990b1883ffcdc5e149e19aa
Sonu Gupta <rg3029926@gmail.com>
2023-01-23 23:50:15 +0530

    es6

---
+++ ecmaScript.js
@@ -0,0 +1,25 @@
+
+// ECMA INTERNATIONAL
+1991
+// It was then submitted to 
+// ECMA INTERNATIONAL for standardization.
+//  which resulted in ECMAScript.
+
+// 2015
+// ES6
+
+// Decided to switch to annual 
+// releases of ECMAScript
+
+
+// Now It's Time for modern JavaScript
+
+// features of ECMAScript 2015
+// also known as ES6;
+// LET VS CONST va VAR
+
+var myName = "Sonu Gupta";
+console.log(myName);
+myName="CEO Sonu Gupta";
+console.log(myName)
+
