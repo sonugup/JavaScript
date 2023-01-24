2d33a3ba64f7875a88e8481c03dffd742c601338
Sonu Gupta <rg3029926@gmail.com>
2023-01-25 02:41:13 +0530

    string

---
+++ stringtype.js
@@ -0,0 +1,23 @@
+let name="Sonu";
+let myName='Sonu Gupta';
+
+console.log(name)
+console.log(myName)
+
+let newName= new String('CEO Sonu Gupta')
+
+console.log(newName)
+
+// const myBiodata= 'I am Sonu Gupta';
+// console.log(myBiodata.indexOf("S"))
+
+const myBiodata= 'I am Sonu Gupta';
+console.log(myBiodata.search("Sonu"));
+
+
+
+
+
+
+
+
