0e22ee8ff1a5412712d2356b691a88bfb76d003e
Sonu Gupta <rg3029926@gmail.com>
2023-01-25 03:11:26 +0530

    charat

--- stringtype.js
+++ stringtype.js
@@ -1,22 +1,36 @@
-let name="Sonu";
-let myName='Sonu Gupta';
+// let name="Sonu";
+// let myName='Sonu Gupta';
 
-console.log(name)
-console.log(myName)
+// console.log(name)
+// console.log(myName)
 
-let newName= new String('CEO Sonu Gupta')
+// let newName= new String('CEO Sonu Gupta')
 
-console.log(newName)
+// console.log(newName)
 
 // const myBiodata= 'I am Sonu Gupta';
 // console.log(myBiodata.indexOf("S"))
 
+// const myBiodata= 'I am Sonu Gupta';
+// console.log(myBiodata.search("Sonu"));
+
+// let str= "Apple, Bananaa, Kiwi";
+
+// // let res=str.slice(0, 4)
+// let res=str.slice(5, -3)
+// console.log(res)
+
+
 const myBiodata= 'I am Sonu Gupta';
-console.log(myBiodata.search("Sonu"));
 
+let repalcedata=myBiodata.replace('am',"Am")
 
+console.log(repalcedata)
 
+let str="HELLO UNIVERSE";
 
+console.log(str.charAt(8))
+console.log(str.charCodeAt(0))
 
 
 
